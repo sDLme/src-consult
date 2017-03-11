fb740713d66b1e9466d87ee4d073a0a01f29a6a8
sDLme <marina2606@gmail.com>
2017-03-11 20:41:06 +0200

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 /**
  * Created by Marina on 10.03.2017.
  */
+
+///testimonials slider
+$(document).ready(function(){
+    $('.slide-testimon').slick({
+        dots: true,
+        autoplay: true,
+        slickNext: false,
+        slickPrev: false,
+})
+    $('.project-img-slide').slick({
+        autoplay: true,
+    })
+    $('.carousel-clients').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        slickNext: true,
+        slickPrev: true
+    })
+});
+
+
