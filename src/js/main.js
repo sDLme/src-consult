539ff4c941297074f90986dd074caeeae9b88b44
sDLme <marina2606@gmail.com>
2017-03-12 22:41:06 +0200

    style

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,20 @@
 
 ///testimonials slider
 $(document).ready(function(){
+
+///testimonials slider
     $('.slide-testimon').slick({
         dots: true,
         autoplay: true,
         slickNext: false,
         slickPrev: false,
-})
+}),
 
+/// Portfolio-slider
     $('.project-img-slide').slick({
-        autoplay: true,
-    })
+        //autoplay: true,
+    }),
+
     ///ABOUT carousel
     $('.carousel-clients').slick({
         infinite: true,
@@ -21,10 +25,11 @@ $(document).ready(function(){
         slidesToScroll: 3,
         slickNext: true,
         slickPrev: true
-    })
-
+    }),
+///intro
     $('.intro-slide').slick({
         infinite: true,
+        autoplay: true,
         slidesToShow: 4,
         slidesToScroll: 2,
         dots: true,
